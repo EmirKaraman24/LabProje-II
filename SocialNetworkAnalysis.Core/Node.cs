c447a2464c99bc145d97c025b311cdd0d4028ebd
EmirKaraman24 <m.emirkrmn24@gmail.com>
2025-12-13 13:45:23 +0300

    Update Node.cs

--- SocialNetworkAnalysis.Core/Node.cs
+++ SocialNetworkAnalysis.Core/Node.cs
@@ -4,5 +4,9 @@ namespace SocialNetworkAnalysis.Core
     {
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
+        public double Activity { get; set; }
+        public double Interaction { get; set; }
+        public double ConnectionCount { get; set; }
+        public List<string> Neighbors { get; set; } = new List<string>();
     }
 }
