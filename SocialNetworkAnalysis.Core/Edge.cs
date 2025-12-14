43e2eefd06eb8dd53005522faeb4c325bd70e505
EmirKaraman24 <m.emirkrmn24@gmail.com>
2025-12-14 10:41:16 +0300

    Update Edge.cs

--- SocialNetworkAnalysis.Core/Edge.cs
+++ SocialNetworkAnalysis.Core/Edge.cs
@@ -2,6 +2,15 @@ namespace SocialNetworkAnalysis.Core
 {
     public class Edge
     {
-        // Properties to be added
+        public string SourceId { get; set; }
+        public string TargetId { get; set; }
+        public double Weight { get; set; }
+
+        public Edge(string sourceId, string targetId, double weight = 0)
+        {
+            SourceId = sourceId;
+            TargetId = targetId;
+            Weight = weight;
+        }
     }
 }
