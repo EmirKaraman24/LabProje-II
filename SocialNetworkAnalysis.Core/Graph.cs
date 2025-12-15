68f0ad2cbf35ab34163a7c25e96baaf80d0a8aaa
EmirKaraman24 <m.emirkrmn24@gmail.com>
2025-12-15 13:30:05 +0300

    Update Graph.cs

--- SocialNetworkAnalysis.Core/Graph.cs
+++ SocialNetworkAnalysis.Core/Graph.cs
@@ -4,6 +4,15 @@ namespace SocialNetworkAnalysis.Core
 {
     public class Graph
     {
-        // Graph implementation to be added
+        public Dictionary<string, Node> Nodes { get; private set; } = new Dictionary<string, Node>();
+        public List<Edge> Edges { get; private set; } = new List<Edge>();
+
+        public void AddNode(Node node)
+        {
+            if (!Nodes.ContainsKey(node.Id))
+            {
+                Nodes[node.Id] = node;
+            }
+        }
     }
 }
