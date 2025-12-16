84c454819ee1c09d35cf9f84da771eb459edfb82
EmirKaraman24 <nuhregaibunsal4114@gmail.com>
2025-12-16 14:25:22 +0300

    Update WeightCalculator.cs

--- SocialNetworkAnalysis.Core/WeightCalculator.cs
+++ SocialNetworkAnalysis.Core/WeightCalculator.cs
@@ -1,7 +1,24 @@
+using System;
+
 namespace SocialNetworkAnalysis.Core
 {
     public static class WeightCalculator
     {
-        // Method to be added
+        public static double CalculateWeight(Node n1, Node n2)
+        {
+            // Formula: 1 / (1 + Sqrt((Act1 - Act2)^2 + (Int1 - Int2)^2 + (Conn1 - Conn2)^2))
+            
+            double diffActivity = n1.Activity - n2.Activity;
+            double diffInteraction = n1.Interaction - n2.Interaction;
+            double diffConnection = n1.ConnectionCount - n2.ConnectionCount;
+
+            double sumOfSquares = (diffActivity * diffActivity) + 
+                                  (diffInteraction * diffInteraction) + 
+                                  (diffConnection * diffConnection);
+
+            double sqrtVal = Math.Sqrt(sumOfSquares);
+            
+            return 1.0 / (1.0 + sqrtVal);
+        }
     }
 }
