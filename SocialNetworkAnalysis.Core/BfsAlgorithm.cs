7c05887a53f82a3d13962a38924c392561d587b7
EmirKaraman24 <nuhregaibunsal4114@gmail.com>
2025-12-22 13:47:48 +0300

    Update BfsAlgorithm.cs

--- SocialNetworkAnalysis.Core/BfsAlgorithm.cs
+++ SocialNetworkAnalysis.Core/BfsAlgorithm.cs
@@ -4,6 +4,36 @@ namespace SocialNetworkAnalysis.Core
 {
     public class BfsAlgorithm
     {
-        // Execute method to be added
+        public List<Node> Execute(Graph graph, string startNodeId)
+        {
+            var visited = new List<Node>();
+            if (!graph.Nodes.ContainsKey(startNodeId)) return visited;
+
+            var queue = new Queue<string>();
+            var visitedIds = new HashSet<string>();
+
+            queue.Enqueue(startNodeId);
+            visitedIds.Add(startNodeId);
+
+            while (queue.Count > 0)
+            {
+                var currentId = queue.Dequeue();
+                if (graph.Nodes.TryGetValue(currentId, out var node))
+                {
+                    visited.Add(node);
+
+                    foreach (var neighborId in node.Neighbors)
+                    {
+                        if (!visitedIds.Contains(neighborId))
+                        {
+                            visitedIds.Add(neighborId);
+                            queue.Enqueue(neighborId);
+                        }
+                    }
+                }
+            }
+
+            return visited;
+        }
     }
 }
