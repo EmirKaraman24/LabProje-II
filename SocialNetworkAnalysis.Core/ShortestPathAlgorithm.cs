54ca243aec6f2506a4ce7b224b28d4ab42e786d8
EmirKaraman24 <m.emirkrmn24@gmail.com>
2025-12-23 08:50:44 +0300

    Create ShortestPathAlgorithm.cs

---
+++ SocialNetworkAnalysis.Core/ShortestPathAlgorithm.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialNetworkAnalysis.Core
+{
+    public class ShortestPathAlgorithm
+    {
+        // Dijkstra and A* methods to be added
+    }
+}
